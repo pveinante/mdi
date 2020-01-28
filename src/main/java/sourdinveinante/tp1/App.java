e48068cf7f4a1da9e01c1f5feb0f3c65399bd9df
Infenix <dun.260@gmail.com>
2020-01-28 16:52:52 +0100

    Ajout projet Maven

---
+++ src/main/java/sourdinveinante/tp1/App.java
@@ -0,0 +1,31 @@
+package sourdinveinante.tp1;
+
+/**
+ * Main Class.
+ * 
+ *@author Duncan
+ */
+public class App {
+	/**
+	 *Main function.
+	 * 
+	 *@author Duncan Paul
+	 *@param args ne sert pas a grand chose dans l'etat
+	 */
+	public static void main(String[] args) {
+    	//Incroyable commentaire
+        System.out.println("Hello World!");
+    }
+    
+    /**
+     * Recuperer la valeur 5, invroyable.
+     * 
+     * @return 5
+     */
+    public static int superFonction()
+    {
+    	return 2 + 5;
+    	
+    	int a = 4;
+    }
+}
